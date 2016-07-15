4bd06c3a227b8acec88b79b4fa78b1f121662fa2
Carlos Galdamez <cgaldamez14@gmail.com>
2016-07-14 18:37:53 -0700

    modified code to allow hpr values

--- platform.py
+++ platform.py
@@ -4,12 +4,17 @@ from panda3d.bullet import BulletRigidBodyNode
 from panda3d.core import Vec3,Texture
 
 class Platform():
+	MODEL = 'models/environ/crate/crate.egg'
+
+	TEXTURES = {'1':'models/textures/grass.jpg',
+		    '2':'models/textures/rock.jpg'}
 	
-	def __init__(self, name, pos, size, mass = 0):
+	def __init__(self, name, size, pos, hpr, mass = 0):
 		self.name = name
 		self.mass = mass
 		(self.x_pos, self.y_pos, self.z_pos) = pos
 		(self.x_size, self.y_size, self.z_size) = size
+		(self.h,self.p,self.r) = hpr
 	
 	def create_bullet_node(self,render,world):
 		self.shape = BulletBoxShape(Vec3(self.x_size,self.y_size,self.z_size))
@@ -19,11 +24,12 @@ class Platform():
             	world.attachRigidBody(self.node)
             	self.np = render.attachNewNode(self.node)
             	self.np.setPos(self.x_pos, self.y_pos, self.z_pos)
+		self.np.setHpr(self.h,self.p,self.r)
 
 	def add_model(self,scale,pos):
 		(x_pos, y_pos, z_pos) = pos
 		(x_scale, y_scale, z_scale) = scale
-		self.model = loader.loadModel('models/environ/crate/crate.egg')
+		self.model = loader.loadModel(Platform.MODEL)
 		self.model.setScale(x_scale,y_scale,z_scale)
                 self.model.setPos(x_pos,y_pos,z_pos)
                 self.model.reparentTo(self.np)
@@ -33,8 +39,5 @@ class Platform():
 		plat_texture.setWrapU(Texture.WM_repeat)
 		plat_texture.setWrapV(Texture.WM_repeat)
                 self.model.setTexture(plat_texture,1)
-
-	def rotateV(self):
-		self.np.setHpr(-20,35,0)
 	
 
