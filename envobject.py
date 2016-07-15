270fe8d3080188c0919864f3a55a6567f31b6113
Carlos Galdamez <cgaldamez14@gmail.com>
2016-07-14 18:39:49 -0700

    added creepy tree to objects to render as collision objects

--- envobject.py
+++ envobject.py
@@ -3,7 +3,7 @@ from panda3d.bullet import BulletTriangleMeshShape
 from panda3d.bullet import BulletTriangleMesh
 
 class EnvObject():
-	models = {
+	MODELS = {
 		'mountains':'models/environ/mountain/mountainegg.egg',
 		'tree1':'models/environ/plant6/plants6.egg',				# Leaf less tree
 		'tree2':'models/environ/plant2/plants2.egg',				# Creepy Tree
@@ -13,23 +13,24 @@ class EnvObject():
 		'purple_flower':'models/environ/shrubbery2/shrubbery2.egg',
 		'red_flower':'models/environ/flower/flower.egg',
 		'wide_ramp' : 'models/environ/wide-ramp/wide-ramp.egg'}
-
-	collisionObjects = []
 	
 	def __init__(self,name, pos, render, world, loader):
 		self.name = name
 		self.render = render
 		self.world = world
 
-		self.model = loader.loadModel(EnvObject.models[self.name])
+		self.model = loader.loadModel(EnvObject.MODELS[self.name])
 		(self.x,self.y,self.z) = pos
 
-	def renderObject(self,scale, collisionOn=False):
+	def renderObject(self,scale,hpr,collisionOn=False):
 		(x_scale,y_scale,z_scale) = scale
+		(h,p,r) = hpr
 		if collisionOn is True:
 			if self.name is 'wide_ramp':
 				(x_c,y_c,z_c) = (x_scale + .2,y_scale+2.5,z_scale+1.75)
 			if self.name is 'tree1':
+				(x_c,y_c,z_c) = (x_scale,y_scale,z_scale)
+			if self.name is 'tree2':
 				(x_c,y_c,z_c) = (x_scale,y_scale,z_scale)
 
        			mesh = BulletTriangleMesh()
@@ -47,10 +48,10 @@ class EnvObject():
 
 			np = self.render.attachNewNode(node)
 			np.setPos(self.x,self.y,self.z)
+			np.setHpr(h,p,r)
 			np.setScale(x_c,y_c,z_c)
 
 			self.world.attachRigidBody(node)
-		#(x_pos,y_pos,z_pos) = position			
 		self.model.setPos(self.x,self.y,self.z)
 		self.model.setScale(x_scale,y_scale,z_scale)
 		self.model.reparentTo(self.render)
